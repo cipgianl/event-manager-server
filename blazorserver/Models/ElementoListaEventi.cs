180cfd4d20c3e81eaf59afc81ac10a2cef72167c
Gianluca Cipolletta <cipollettagianluca@gmail.com>
2022-04-19 17:15:31 +0200

    Aggiungere i file di progetto.

---
+++ blazorserver/Models/ElementoListaEventi.cs
@@ -0,0 +1,10 @@
+﻿namespace blazorserver.Models
+{
+    public class ElementoListaEventi
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Localita { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
