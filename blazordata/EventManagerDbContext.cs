25bdb9b581ee375b4b771d3d5f37bad05663756c
Gianluca Cipolletta <cipollettagianluca@gmail.com>
2022-04-19 17:57:17 +0200

    Persistenza dei dati a Blazor Server

---
+++ blazordata/EventManagerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blazordata
+{
+    public class EventManagerDbContext: DbContext
+    {
+        public DbSet<DatiEvento> Eventi { get; set; }
+
+        public EventManagerDbContext(DbContextOptions options) : base(options) { }
+
+    }
+}
--- blazorserver/Program.cs
+++ blazorserver/Program.cs
@@ -1,11 +1,15 @@
+using blazordata;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+builder.Services.AddDbContext<EventManagerDbContext>(opt => opt.UseSqlite("DataSource=eventmanager.db"));
 
 var app = builder.Build();
 
